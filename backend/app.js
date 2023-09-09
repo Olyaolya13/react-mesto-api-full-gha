e2c3339bdc272d74aefaae58b01f479008c0a9af
Olga Pankrashina <olyaolya2713@yandex.ru>
2023-09-09 22:07:59 +0300

    test

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const { UrlPattern } = require('./errors/constants/constants');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
-const { PORT = 3001, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 app.use(cors());
 
 app.use(helmet()); // middleware для усиления безопасности
